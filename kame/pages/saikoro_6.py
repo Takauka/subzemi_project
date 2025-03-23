44d338b4efb62138cdf9b9c6081843c9cd28c8b6
Takauka <k22m2022@cis.twcu.ac.jp>
2025-03-23 16:16:41 +0900

    add file

---
+++ kame/pages/saikoro_6.py
@@ -0,0 +1,28 @@
+import random
+import streamlit as st
+import pandas as pd
+
+#if "omikuji" not in st.session_state:
+   #st.session_states.omikuji = []
+
+st.title("おみくじ")
+
+if st.button("おみくじをひく"):
+    result = random.randint(1, 6)
+    #st.session_state.omikuji.append((result))
+    if result == 1:
+       st.write(f"{result}番 大吉です。")
+    elif result == 2:
+       st.write(f"{result}番 中吉です。")
+    elif result == 3:
+       st.write(f"{result}番 小吉です。")
+    elif result == 4:
+       st.write(f"{result}番 末吉です。")
+    elif result == 5:
+       st.write(f"{result}番  凶です。")
+    else:
+       st.write(f"{result}番 大凶 です。")
+
+#df = pd.DataFrame(st.session_state.omikuji, colums=["おみくじ番号"])
+#st.dateframe(df)
+#st.write("おみくじをひいた回数", len(st.session_state.omikuji))
